835f02d320443de98885f9d453f4482b3127ddd3
SuzanneNL <skr470@gmail.com>
2021-01-06 16:56:18 +0000

    Add send message functionality

---
+++ js/email.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "solarcan", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
